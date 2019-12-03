7494818c73acdf3c1b2328c61c30e6ffc7f1a293
lesterhnu <lesterhnu@163.com>
2019-12-03 16:33:35 +0800

    重新初始化

--- resources/js/api/index.js
+++
@@ -1,6 +0,0 @@
-//import request from '../utils/index'
-import axios from 'axios'
-// axios.defaults.baseURL('http://www.blog.com/api')
-export var getArticleList = function(cate_id){
-    return axios.get(`http://www.blog.com/api/getArticleList`,cate_id).then(res=>res.data)
-}
